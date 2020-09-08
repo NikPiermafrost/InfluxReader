c7ed988a3efec8a01773629ab41cba4840b84f5a
Nicola Piermattei <nicola.piermattei@beantech.it>
2020-09-08 10:42:56 +0200

    data now loaded by configuration

---
+++ influx-fe/Models/ConfigModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace influx_fe.Models
+{
+    public class ConfigModel
+    {
+        public List<string> DataTables { get; set; }
+    }
+}
